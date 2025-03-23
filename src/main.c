65cfb23b67d05de3f5ed1c317319747624d0477b
Laurentiu Turcu <turculaurentiu91@gmail.com>
2025-03-23 16:01:53 +0100

    feat: added response header

--- src/main.c
+++ src/main.c
@@ -1,11 +1,40 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#define HF_RECURSION_DESIRED 0b0000000100000000   // 0000 0001 0000 0000
+#define HF_RECURSION_AVAILABLE 0b0000000010000000 // 0000 0000 1000 0000
+#define HF_TRUNCATED_MESSAGE 0b0000001000000000   // 0000 0010 0000 0000
+#define HF_AUTHORITATIVE_ANS 0b0000010000000000   // 0000 0100 0000 0000
+#define HF_QUERY_RESPONSE 0b1000000000000000      // 1000 0000 0000 0000
+
+struct header_t {
+  uint16_t id;
+  uint16_t flags;
+  uint16_t qdcount;
+  uint16_t ancount;
+  uint16_t nscount;
+  uint16_t arcount;
+};
+
+/**
+ * The out array must have space for 12 uint8_t or it will access memory out of
+ * bounds
+ */
+void header_to_net(struct header_t header, uint16_t *out) {
+  out[0] = htons(header.id);
+  out[1] = htons(header.flags);
+  out[2] = htons(header.qdcount);
+  out[3] = htons(header.ancount);
+  out[4] = htons(header.nscount);
+  out[5] = htons(header.arcount);
+}
 
 int main() {
   setbuf(stdout, NULL);
@@ -59,7 +88,17 @@ int main() {
     printf("Received %d bytes: %s\n", bytesRead, buffer);
 
     // Create an empty response
-    char response[1] = {'\0'};
+    uint8_t response[12];
+    struct header_t res_header = {
+        .id = 1234,
+        .flags = HF_QUERY_RESPONSE,
+        .qdcount = 0,
+        .ancount = 0,
+        .nscount = 0,
+        .arcount = 0,
+    };
+
+    header_to_net(res_header, (uint16_t *)response);
 
     // Send response
     if (sendto(udpSocket, response, sizeof(response), 0,
